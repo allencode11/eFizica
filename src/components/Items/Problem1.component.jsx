297cc6735c483384bdd0cc0b18ae4f3e4dcab082
allencode11 <al.el.en.lina@gmail.com>
2022-04-02 22:06:20 +0300

    fixing problem one and two components

--- src/components/Items/Problem1.component.jsx
+++ src/components/Items/Problem1.component.jsx
@@ -1,8 +1,26 @@
+import { MathComponent } from 'mathjax-react';
 
 export const FirstProblemItem = ({item}) => {
+  const condition = item.condition[0].split('@');
+
   return (
     <div>
-      <div>{item.condition[0]}</div>
+      {
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center'}}>
+          {
+            condition.map((element, index) => (
+              <div>
+                {
+                  index % 2 === 0 ?
+                    <span style={{margin: 3}}> {element} </span> :
+                    <span style={{ width: 0, backgroundColor: 'red', display: 'inline-block'}}>
+                    <MathComponent tex={element}/>
+                  </span>
+                }
+              </div>
+            ))}
+        </div>
+      }
       {[
         ...Array(Number(item.condition[1])),
       ].map((value, index) => (
--- src/components/Items/Problem2.component.jsx
+++ src/components/Items/Problem2.component.jsx
@@ -1,8 +1,27 @@
+import { MathComponent } from 'mathjax-react';
+
 export const SecondProblemItem = ({item}) => {
+  const condition = item.condition[0].split('@').filter(element => element != '');
   return (
     <div style={{flexDirection: 'column'}}>
-      <div>{item.condition[0]}</div>
-      <img style={{width: `${item.condition[2]}`, height: 'auto', paddingBottom: 5}} src={item.condition[1]}/>
+      {
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center'}}>
+          {
+          condition.map((element, index) => (
+            <div>
+              {
+                index % 2 === 0 ?
+                  <span style={{margin: 3}}> {element} </span> :
+                  <span style={{ width: 0, backgroundColor: 'red', display: 'inline-block'}}>
+                    <MathComponent tex={element}/>
+                  </span>
+              }
+            </div>
+          ))}
+        </div>
+      }
+      <img
+        style={{width: `${item.condition[2]}`, display: 'flex', flexDirection: 'row', alignItems:'right', height: 'auto', paddingBottom: 5}} src={item.condition[1]}/>
       {[
         ...Array(Number(item.condition[3])),
       ].map((value, index) => (
